1e63b2af4f20c49c6d18696805107624621517c7
UmerYousaf0260 <umeryousaf415@gmail.com>
2022-08-06 13:27:54 +0500

    updated

--- myportfolio/app.js
+++ myportfolio/app.js
@@ -1,3 +1,33 @@
+//Responsive nav
+
+const respNav = () => {
+
+    const navBtn = document.querySelector('.menu-bar');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    navBtn.addEventListener('click', function () {
+
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, i) => {
+
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+
+            else {
+                link.style.animation = `navLinkAnimation 0.5s ease forwards ${i / 7 + 0.42}s`;
+            }
+
+        });
+
+        navBtn.classList.toggle('menu-cross');
+    });
+}
+
+respNav();
+
 //Navbar animation
 const sections = document.querySelectorAll('section');
 const bubble = document.querySelector('.bubble');
@@ -93,11 +123,11 @@ $(".carousel").owlCarousel({
             items: 1,
             nav: false
         },
-        600: {
+        700: {
             items: 2,
             nav: false
         },
-        1000: {
+        1100: {
             items: 3,
             nav: false
         }
